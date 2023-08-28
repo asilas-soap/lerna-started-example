972617c90af203b22760577a03ec2c49398c06eb
Alberto Silas <asilas@soap.health>
2023-08-28 11:00:52 -0300

    fix: checkout

--- deployment-pipeline/index.js
+++ deployment-pipeline/index.js
@@ -16,6 +16,7 @@ async function runSteps(error, bumpRecommendation) {
   const bump = bumpVersion(filePackageJson, bumpRecommendation.releaseType);
 
   await git2.init();
+  await git2.checkout(branch);
   await git2.pull();
 
   const newVersion = bump.getNextVersion();
