21daf31abca829e4b8811575f0d6259c58fa0d89
Conventional commit log <asilas@soap.health>
2023-08-28 17:06:28 -0300

    fix: gia

--- deployment-pipeline/index.js
+++ deployment-pipeline/index.js
@@ -17,14 +17,14 @@ async function runSteps(error, bumpRecommendation) {
 
   const branch = "main";
 
-  // await git.config("user.name", "Alberto Silas");
-  // await git.config("user.email", "asilas@soap.health");
+  await git.config("user.name", "Alberto Silas");
+  await git.config("user.email", "asilas@soap.health");
   
   const bump = bumpVersion(filePackageJson, bumpRecommendation.releaseType);
 
-  await git2.init();
-  await git2.checkout(branch);
-  await git2.pull();
+  // await git2.init();
+  // await git2.checkout(branch);
+  // await git2.pull();
 
   const newVersion = bump.getNextVersion();
   const result = await generateChangelog(fileChangelog, newVersion, "v");
@@ -32,15 +32,15 @@ async function runSteps(error, bumpRecommendation) {
 
   bump.updateToNextVersion();
 
-  // await git.add(".");
-  // await git.commit(`chore: ${newVersion}`);
-  // await git.createTag(newVersion);
+  await git.add(".");
+  await git.commit(`chore: ${newVersion}`);
+  await git.createTag(newVersion);
 
-  // await git.push();
-  await git2.add(".");
-  await git2.commit(`chore(release): ${newVersion}`);
-  await git2.createTag(`v${newVersion}`);
-  await git2.push(branch);
+  await git.push();
+  // await git2.add(".");
+  // await git2.commit(`chore(release): ${newVersion}`);
+  // await git2.createTag(`v${newVersion}`);
+  // await git2.push(branch);
 
   
 }
