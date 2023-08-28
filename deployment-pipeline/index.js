78af9247b5c049ad29ee0e0d5f5a42d99019d39a
Conventional commit log <asilas@soap.health>
2023-08-28 14:49:22 -0300

    fix: git

--- deployment-pipeline/index.js
+++ deployment-pipeline/index.js
@@ -15,9 +15,9 @@ async function runSteps(error, bumpRecommendation) {
 
   const bump = bumpVersion(filePackageJson, bumpRecommendation.releaseType);
 
-  await git2.init();
-  await git2.checkout(branch);
-  await git2.pull();
+  // await git2.init();
+  // await git2.checkout(branch);
+  // await git2.pull();
 
   const newVersion = bump.getNextVersion();
   const result = await generateChangelog(fileChangelog, newVersion, "v");
@@ -25,10 +25,10 @@ async function runSteps(error, bumpRecommendation) {
 
   bump.updateToNextVersion();
 
-  await git2.add(".");
-  await git2.commit(`chore(release): ${newVersion}`);
-  await git2.createTag(`v${newVersion}`);
-  await git2.push(branch);
+  // await git2.add(".");
+  // await git2.commit(`chore(release): ${newVersion}`);
+  // await git2.createTag(`v${newVersion}`);
+  // await git2.push(branch);
 }
 
 conventionalRecommendedBump({ preset: `angular` }, runSteps);
