a44cba97f4bf36316063ff8fee2438766a21ff08
Alberto Silas <asilas@soap.health>
2023-09-13 13:13:26 -0300

    chore: refactored code

--- deployment-pipeline/tasks.js
+++ deployment-pipeline/tasks.js
@@ -1,5 +1,5 @@
-import { readFileSync } from "fs";
-import { checkAndUpdate, updateStatus } from "./utils/clickUp.js";
+import { readFileSync, existsSync } from "fs";
+import { checkAndUpdate } from "./utils/clickUp.js";
 
 function getTasksId(text) {
   return text.replace("\n", " ").split(" ")
@@ -16,26 +16,45 @@ function validateStatus(status) {
   throw new Error(`Status ${status} is not valid.`);
 }
 
+// Main function ------------------------------------------------------------------------------------------------------
+
+/**
+ * Reads a file with a commit message, collects all Click Up tasks mentioned
+ * and updated their status using Click Up API.
+ * 
+ * @param {*} path File that contains the commit message.
+ * @param {*} currentStatus Expected current status of the Click Up Tasks.
+ * @param {*} newStatus New status to be updated for the Click Up Tasks.
+ * @returns 
+ */
 function updateTasks(path, currentStatus, newStatus) {
+  if (!existsSync(path)) {
+    throw Error(`File with the commit message not found: ${path}`);
+  }
+
   const content = readFileSync(path).toString();
   const ids = getTasksId(content);
 
+  if (!ids || ids.length == 0) {
+    console.log("SKIP: None Click Up tasks ids found to update.")
+    return;
+  }
   console.log(currentStatus, newStatus);
   console.log("Updating Click Up tasks. Tasks Ids:", ids, "New Status:", newStatus);
 
   const list = [];
   ids.forEach(element => {
-
     const req = checkAndUpdate(element, currentStatus, newStatus);
     list.push(req);
   });
 
   Promise.all(list)
     .then(
-      (value) => console.log('All requests finished.'),
+      () => console.log('All requests were finished.'),
       (reason) => console.log('Request was rejected: ', reason));
 }
 
 const currentStatus = validateStatus(process.argv[2]);
 const newStatus = validateStatus(process.argv[3]);
+
 updateTasks("/root/.ssh/log.txt", currentStatus, newStatus);
