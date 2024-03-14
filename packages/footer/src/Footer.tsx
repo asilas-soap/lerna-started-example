94d8bb586333946391ee323ecfc8396f91b2bcc4
Alberto Silas <asilas@soap.health>
2024-03-14 09:10:19 -0300

    chore: no changes

--- packages/footer/src/Footer.tsx
+++ packages/footer/src/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Footer() {
     return (
         <footer>
-            Footer component test commit
+            Footer component
         </footer>
     );
 }
