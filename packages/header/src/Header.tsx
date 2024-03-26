78e33bf6ebef0d971f3cacc9d0cb4b80b11a7b4b
Alberto Silas <asilas@soap.health>
2024-03-26 09:33:17 -0300

    fix(header): remove force

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -5,7 +5,6 @@ export function Header() {
         <header>
             Lerna is the original monorepo tool! Enjoy this amazing tool.
             Force commit.
-            Force.
         </header>
     );
 }
